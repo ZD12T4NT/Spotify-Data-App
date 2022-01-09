89584c1fee9b4049939249fb13470fc5cfc01b0c
Zac Distant <zacdistant@gmail.com>
2022-01-09 12:59:16 +0000

    make it better

---
+++ client/src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as Login } from './login';
+export { default as Profile } from './profile';
+export { default as TopArtists } from './TopArtists';
+export { default as TopTracks } from './TopTracks';
+export { default as Playlists } from './Playlists';
+export { default as Playlist } from './Playlist';
